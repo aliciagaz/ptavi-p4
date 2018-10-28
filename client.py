142145332b990bb1b6eb7968a6dbb88a3297b435
Alicia Gordo Azabal <43533576+aliciagaz@users.noreply.github.com>
2018-10-28 10:55:53 +0100

    Ejercicio 4 terminado

--- client.py
+++ client.py
@@ -4,12 +4,10 @@
 import socket
 import sys
 
-# Constantes. Dirección IP del servidor y contenido a enviar
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
 LINE = " ".join(sys.argv[3:])
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
     print("Enviando:", LINE)
