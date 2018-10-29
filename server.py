78cfad2a4ae0a4a98f1562f004c775ee586e8dae
Alicia Gordo Azabal <43533576+aliciagaz@users.noreply.github.com>
2018-10-29 09:29:54 +0100

    Ejercicio 6 terminado

--- server.py
+++ server.py
@@ -5,15 +5,20 @@ import socketserver
 import sys
 
 class EchoHandler(socketserver.DatagramRequestHandler):
-    
+    dic_register= {}
     def handle(self):
-        
-        address = self.client_address[0]
-        port = self.client_address[1]
         self.wfile.write(b"Hemos recibido tu peticion")
         for line in self.rfile:
-            print("Cliente con IP " + str(address) +" y puerto " + str(port))
-            print("El cliente manda: ", line.decode('utf-8'))
+            msg = line.decode('utf-8')
+            print(msg)
+            lista_msg = msg.split(" ")
+            address = self.client_address[0]
+            port = self.client_address[1]
+            if lista_msg[0] == "REGISTER":
+                direction= lista_msg[1][lista_msg[1].rfind(":") +1:]
+                self.dic_register[direction]= [address]
+                print("Cliente con IP " + str(address) +" y puerto " + str(port))
+        
 if __name__ == "__main__":
     
     PORT = int(sys.argv[1])
