75df8374cdfe826d77bd066d48faae3b50a2024a
Alicia Gordo Azabal <43533576+aliciagaz@users.noreply.github.com>
2018-10-29 09:29:24 +0100

    Ejercicio 6 terminado

--- client.py
+++ client.py
@@ -6,13 +6,16 @@ import sys
 
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
-LINE = " ".join(sys.argv[3:])
+REGISTER = sys.argv[3]
+DIRECTION = sys.argv[4]
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
-    print('Recibido -- ', data.decode('utf-8'))
-
+    if REGISTER == "register":
+        MSG = ("REGISTER sip:" + DIRECTION + " SIP/2.0\r\n")
+        print("Enviando:", MSG)
+        my_socket.send(bytes(MSG, 'utf-8') + b'\r\n')
+        data = my_socket.recv(1024)
+        print('Recibido -- ', data.decode('utf-8'))
+      
 print("Socket terminado.")
