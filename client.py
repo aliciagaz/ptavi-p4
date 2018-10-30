ccb2b156508f464c7028b6e3ef69c181e0f6125c
Alicia Gordo Azabal <43533576+aliciagaz@users.noreply.github.com>
2018-10-30 18:00:12 +0100

    Ejercicio 7 terminado

--- client.py
+++ client.py
@@ -4,15 +4,21 @@
 import socket
 import sys
 
-SERVER = sys.argv[1]
-PORT = int(sys.argv[2])
-REGISTER = sys.argv[3]
-DIRECTION = sys.argv[4]
+try:
+    SERVER = sys.argv[1]
+    PORT = int(sys.argv[2])
+    REGISTER = sys.argv[3]
+    DIRECTION = sys.argv[4]
+    EXPIRES = sys.argv[5]
+    
+except:
+    sys.exit("Usage: client.py ip puerto register sip_address expires_value")
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
     if REGISTER == "register":
-        MSG = ("REGISTER sip:" + DIRECTION + " SIP/2.0\r\n")
+        MSG = ("REGISTER sip:" + DIRECTION + " SIP/2.0\r\n" + "Expires: " +
+               str(EXPIRES) + "\r\n\r\n")
         print("Enviando:", MSG)
         my_socket.send(bytes(MSG, 'utf-8') + b'\r\n')
         data = my_socket.recv(1024)
