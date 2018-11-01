c0b8a54022d912564d58162e066d009fd033b477
Alicia Gordo Azabal <43533576+aliciagaz@users.noreply.github.com>
2018-11-01 18:15:57 +0100

    Práctica completa (con PEP8 y docstrings)

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
+"""
+Programa cliente UDP que abre un socket a un servidor
+"""
 import socket
 import sys
 
@@ -10,18 +12,19 @@ try:
     REGISTER = sys.argv[3]
     DIRECTION = sys.argv[4]
     EXPIRES = sys.argv[5]
-    
-except:
+
+except IndexError:
     sys.exit("Usage: client.py ip puerto register sip_address expires_value")
 
+# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
     if REGISTER == "register":
-        MSG = ("REGISTER sip:" + DIRECTION + " SIP/2.0\r\n" + 
+        MSG = ("REGISTER sip:" + DIRECTION + " SIP/2.0\r\n" +
                "Expires: " + str(EXPIRES) + "\r\n\r\n")
         print("Enviando:", MSG)
         my_socket.send(bytes(MSG, 'utf-8') + b'\r\n')
         data = my_socket.recv(1024)
         print('Recibido -- ', data.decode('utf-8'))
-      
+
 print("Socket terminado.")
--- server.py
+++ server.py
@@ -1,56 +1,74 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
+"""
+Programa principal para un servidor UDP simple
+"""
 import socketserver
 import sys
 import time
 import json
 
-class SIPRegisterHandler(socketserver.DatagramRequestHandler):
-    dic_register= {}
-    
-    def register2json(self):
-        json_file = open ("registered.json", "w")
-        json.dump(self.dic_register, json_file)
-        json_file.close()
 
-    def json2registered(self): 
+class SIPRegisterHandler(socketserver.DatagramRequestHandler):
+    dic_register = {}
+
+    def register2json(self):
+        """
+        Convierte nuestro registro (diccionario) en un fichero JSON
+        """
+        fich_json = open("registered.json", "w")
+        json.dump(self.dic_register, fich_json)
+        fich_json.close()
+
+    def json2registered(self):
+        """
+        Comprueba que haya un fichero llamado registered.json y si existe,
+        lee su contenido y lo mete en un diccionario de usuarios registrados
+        """
         try:
-            with open ("registered.json", "r") as json_file:
-               self.dic_register = json.load(json_file)
+            with open("registered.json", "r") as fich_json:
+                self.dic_register = json.load(fich_json)
         except FileNotFoundError:
             self.dic_register = {}
 
     def time_out(self):
+        """
+        Comprueba si algún usuario ha caducado y si es así, lo borra del
+        diccionario
+        """
         lista = list(self.dic_register)
         for client in lista:
             time_expires = self.dic_register[client][1]
-            gmt_actual = time.strftime("%Y-%m-%d %H:%M:%S", 
-                                        time.gmtime(time.time()))
+            gmt_actual = time.strftime("%Y-%m-%d %H:%M:%S",
+                                       time.gmtime(time.time()))
             if time_expires < gmt_actual:
                 del self.dic_register[client]
 
     def handle(self):
+        """
+        Es el manejador del servidor, cuando llega un mensaje del cliente
+        éste lo procesa. Si empieza por REGISTER guarda el usuario en nuestro
+        diccionario y si empieza por EXPIRE podemos ver el tiempo de caducidad
+        """
         self.wfile.write(b"Hemos recibido tu peticion")
         for line in self.rfile:
             msg = line.decode('utf-8')
-            
+
             lista_msg = msg.split(" ")
             address = self.client_address[0]
             port = self.client_address[1]
-            
+
             if lista_msg[0] == "REGISTER":
-                direction= lista_msg[1][lista_msg[1].rfind(":") +1:]
+                direction = lista_msg[1][lista_msg[1].rfind(":") + 1:]
                 print("\n" + "--> " + "Cliente con IP " + str(address) +
                       " y puerto " + str(port))
                 print("\n" + "Envía: " + msg)
-                
             elif lista_msg[0] == "Expires:":
                 expires = lista_msg[1]
-                gmt_expires = time.strftime("%Y-%m-%d %H:%M:%S", 
-                                        time.gmtime(time.time() + 
-                                                    int(expires)))
-                self.dic_register[direction]= [address, gmt_expires]
+                gmt_expires = time.strftime("%Y-%m-%d %H:%M:%S",
+                                            time.gmtime(time.time() +
+                                                        int(expires)))
+                self.dic_register[direction] = [address, gmt_expires]
                 print("Expire: " + expires)
                 if int(expires) == 0:
                     del self.dic_register[direction]
@@ -59,10 +77,11 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         print(self.dic_register)
         print("----------------------------------------------------------")
 
+
 if __name__ == "__main__":
-    
+
     PORT = int(sys.argv[1])
-    serv = socketserver.UDPServer(('', PORT), SIPRegisterHandler) 
+    serv = socketserver.UDPServer(('', PORT), SIPRegisterHandler)
 
     print("Lanzando servidor UDP de eco...")
     try:
